6aa4b6b865ddfea6e44784f18695acbb3b11f1a8
VinnieVenture <57813811+VinnieVenture@users.noreply.github.com>
2021-02-10 18:14:45 +0100

    Revert "Install chromely"

--- Przypominajka/Program.cs
+++ Przypominajka/Program.cs
@@ -1,7 +1,4 @@
-﻿using Chromely;
-using Chromely.Core;
-using Chromely.Core.Configuration;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,38 +6,11 @@ using System.Threading.Tasks;
 
 namespace Przypominajka
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            // create a configuration with OS-specific defaults
-            var config = DefaultConfiguration.CreateForRuntimePlatform();
 
-            // your configuration
-            config.StartUrl = AppDomain.CurrentDomain.BaseDirectory +$"net461\\ClientApp\\src\\index.html";
-            config.WindowOptions.Title = "Przypominajka";
-            config.WindowOptions.WindowFrameless = true;
-            config.WindowOptions.StartCentered = false;
-            config.WindowOptions.Size = new WindowSize(500, 800);
-            config.WindowOptions.Position = new WindowPosition(1350, 200);
-            //..
-
-            // application builder
-            AppBuilder
-            .Create()
-            .UseApp<DemoChromelyApp>()
-            .UseConfig<IChromelyConfiguration>(config)
-            .Build()
-            .Run(args);
         }
-    }
-
-    public class DemoChromelyApp : ChromelyBasicApp
-    {
-        //public override void ConfigureServices(IServiceCollection services)
-        //{
-        //    base.ConfigureServices(container);
-        //    // other service configuration can be placed here
-        //}
     }
 }
